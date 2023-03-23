91bc4b65d4d623455a175c104cbc41a9cba1056c
FoXy <12250394+theonlyfoxy@users.noreply.github.com>
2023-03-23 23:28:37 +0330

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud",
-      version = "= 1.27.2"
+      version = "= 1.36.2"
     }
   }
-  required_version = "= 1.0.1"
+  required_version = "= 1.4.2"
 }
